875564dc6b56e60ac193c16c2ec60dc06d3a7d50
kyle1james <kyle1james@gmail.com>
2023-10-25 21:49:23 -0400

    neuron done

--- utils/neuronV2.py
+++ utils/neuronV2.py
@@ -36,19 +36,20 @@ class Neuron:
         to bound answer within -1-1 range, introduces non-linearity
         '''
         # 2x3 * 3x1
-        output = np.dot(self.weights, parent_node.signal)
+        output = self.activate(np.dot(self.weights, parent_node.signal))
         # 2x3
         delta_i, delta_s = list(output)
         self.input = delta_i[0]
         self.state = delta_s[0]
-        print(
-            f'output {output} and weights {self.weights} and state {self.state}')
-        print('')
+
         # 2x3
-        self.weights = self.activate(output)
+        self.weights = output  # self.activate(output)
 
         self.signal = np.array([self.input, self.state, 1]).reshape(-1, 1)
         # recurrent (system dynamics by feeding input -> output -> input)
+        print(
+            f'output {output} and weights {self.weights} and state {self.state}')
+        print('')
 
     def backprop(self):
         pass
